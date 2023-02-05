339650382c51108ec534327d076a9e7c6f9ecb3f
sempedia <104672255+sempedia@users.noreply.github.com>
2023-02-06 00:07:17 +0200

    Completed module

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import BlogPost
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+
+def index(): 
+    posts = BlogPost.objects.all()
+    return get_object_or_404(BlogPost, pk=id)
