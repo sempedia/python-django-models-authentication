5a83bedfe43a41733db2833dff5d18feba62bfcb
sempedia <104672255+sempedia@users.noreply.github.com>
2023-02-06 01:32:28 +0200

    Completed module

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
-from .models import BlogPost
+
 
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
-admin.site.register(BlogPost)
